3cfec4ade9ef7c86ca33f5263efb28b975c0abee
Tamara Cvjetkovic <c.tamara02@gmail.com>
2023-04-14 23:41:56 +0200

    [FEAT] page 'O nama'

---
+++ PROJEKAT - TURISTICKE AGENCIJE/js/aboutus.js
@@ -0,0 +1,75 @@
+
+window.addEventListener('load', main);
+
+
+function main() {
+
+    // ***** CHANGING NAVBAR WHEN SCROLLED *****
+    document.addEventListener("scroll", () => {
+        let nav = document.querySelector(".navbar");
+        let nava = document.querySelectorAll(".nava");
+        if (window.scrollY > 0) {
+            nav.classList.add("scrolled");
+            for (let i = 0; i < nava.length; i++) {
+                nava[i].classList.add("scrolled");
+            }
+        }
+        if (window.scrollY == 0) {
+            nav.classList.remove("scrolled");
+            for (let i = 0; i < nava.length; i++) {
+                nava[i].classList.remove("scrolled");
+            }
+        }
+    });
+
+    // ***** GO-UP BUTTON *****
+    document.addEventListener("scroll", () => {
+        let goUp = document.querySelector(".go-up");
+        if (window.scrollY > 0) {
+            goUp.classList.add("scrolled");
+        }
+        if (window.scrollY == 0) {
+            goUp.classList.remove("scrolled");
+        }
+    });
+}
+
+function scrollToTop() {
+    var position = document.body.scrollTop || document.documentElement.scrollTop;
+    if (position) {
+      window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
+      scrollAnimation = setTimeout("scrollToTop()", 12);
+    } else clearTimeout(scrollAnimation);
+  }
+
+
+
+
+
+/*
+var res = 0;
+var okay = 0;
+
+function izracunajRezultat() {
+    let br1 = document.querySelector('#br1');
+    let br2 = document.querySelector('#br2');
+    res = Number(br1.value) + Number(br2.value);
+    document.querySelector('#rezultat').innerHTML = `Rezultat je <b> ${res} </b>!`;
+    okay = 1;
+}
+
+function promijeniParagraf() {
+    let p1 = document.querySelector('#paragraf1');
+    let vr = res;
+    if (okay == 1) {
+        p1.innerHTML = `<b> Vrijednost prethodne racunice je ${vr}! </b>`;
+    } else {
+        p1.innerHTML = `<b> Prvo unesite neki broj! </b>`; 
+    }
+}
+function misNijeTu() {
+    document.querySelector('#paragraf1').innerHTML = `Racunica za prethodno!`;
+}
+
+
+*/
--- PROJEKAT - TURISTICKE AGENCIJE/js/function.js
+++ PROJEKAT - TURISTICKE AGENCIJE/js/function.js
@@ -1,10 +1,26 @@
 
 window.addEventListener('load', main);
 
+var curBox = 2;
+var firebaseUrl = 'https://turistickaagencijaprojekattaca-default-rtdb.europe-west1.firebasedatabase.app';
+
+var agenciesID = [];
+var agencies = {};
+
+
+function createMainBox() {
+    let newMainBox = document.createElement('div');
+    newMainBox.classList.add("box");
+    newMainBox.innerHTML = " " + curBox + " ";
+    let mainBoxes = document.querySelector(".boxes");
+    mainBoxes.appendChild(newMainBox);
+    curBox++;
+};
 
 
 function main() {
 
+    
     // ***** CHANGING NAVBAR WHEN SCROLLED *****
     document.addEventListener("scroll", () => {
         let nav = document.querySelector(".navbar");
@@ -33,6 +49,16 @@ function main() {
             goUp.classList.remove("scrolled");
         }
     });
+
+
+
+    //alert("Pravite novo dugme!");
+
+    /*var request = new XMLHttpRequest();*/
+    for (let i = 0; i < 8; i++) {
+        createMainBox();
+    }
+
 }
 
 function scrollToTop() {
@@ -42,6 +68,21 @@ function scrollToTop() {
       scrollAnimation = setTimeout("scrollToTop()", 12);
     } else clearTimeout(scrollAnimation);
   }
+
+
+
+function removeMainBoxes(elem) {
+    var elem = document.querySelector(elem);
+    while (elem.firstChild) {
+        elem.removeChild(elem.lastChild);
+    }
+}
+
+
+
+
+
+
 
 
 
