c7328bbd518f77a41e73aca8972c90080b772645
Tamara Cvjetkovic <c.tamara02@gmail.com>
2023-04-15 18:44:46 +0200

    [FEAT] import agencies on main page from Firebase

--- PROJEKAT - TURISTICKE AGENCIJE/js/function.js
+++ PROJEKAT - TURISTICKE AGENCIJE/js/function.js
@@ -1,13 +1,19 @@
 
 window.addEventListener('load', main);
 
-var curBox = 2;
-var firebaseUrl = 'https://turistickaagencijaprojekattaca-default-rtdb.europe-west1.firebasedatabase.app';
+/*
 
-var agenciesID = [];
-var agencies = {};
+***** RANDOM STUFF *****
 
+//alert("Pravite novo dugme!");
 
+for (let i = 0; i < 8; i++) {
+    createMainBox();
+}
+
+*/
+
+var curBox = 0;
 function createMainBox() {
     let newMainBox = document.createElement('div');
     newMainBox.classList.add("box");
@@ -17,10 +23,24 @@ function createMainBox() {
     curBox++;
 };
 
+function scrollToTop() {
+    var position = document.body.scrollTop || document.documentElement.scrollTop;
+    if (position) {
+      window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
+      scrollAnimation = setTimeout("scrollToTop()", 12);
+    } else clearTimeout(scrollAnimation);
+}
+
+
+
+var firebaseUrl = 'https://turistickaagencijaprojekattaca-default-rtdb.europe-west1.firebasedatabase.app';
+
+var agenciesID = [];
+var agencies = {};
+
 
 function main() {
 
-    
     // ***** CHANGING NAVBAR WHEN SCROLLED *****
     document.addEventListener("scroll", () => {
         let nav = document.querySelector(".navbar");
@@ -50,33 +70,118 @@ function main() {
         }
     });
 
-
-
-    //alert("Pravite novo dugme!");
-
-    /*var request = new XMLHttpRequest();*/
-    for (let i = 0; i < 8; i++) {
-        createMainBox();
-    }
-
+    
+    // ***** LOAD BOXES ON MAIN PAGE *****
+    loadBoxes();
 }
 
-function scrollToTop() {
-    var position = document.body.scrollTop || document.documentElement.scrollTop;
-    if (position) {
-      window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
-      scrollAnimation = setTimeout("scrollToTop()", 12);
-    } else clearTimeout(scrollAnimation);
-  }
 
 
+function loadBoxes() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                //removeMainBoxes(".box");
+                agenciesID = [];
+                curBox = 0;
+                agencies = JSON.parse(request.responseText);
+                for (var id in agencies) {
+                    var agency = agencies[id];
+                    appendMainBox(".boxes", agency);
+                    agenciesID.push(id);
+                }
+            } else {
+                alert('Error occurred. Car could not be loaded.')
+            }
+        }
+    }
+    request.open('GET', firebaseUrl + '/agencjie.json');
+    request.send();
+}
 
-function removeMainBoxes(elem) {
-    var elem = document.querySelector(elem);
+
+function appendMainBox(position, agency) {
+
+    let newMainBox = document.createElement('div');
+    newMainBox.classList.add("box");
+    curBox++;
+    newMainBox.setAttribute('onclick', 'ocitajStranicu(this.id)');
+    newMainBox.setAttribute('id', curBox);
+
+    let newBoxCard = document.createElement('div');
+    newBoxCard.classList.add("card");  
+
+    let newAgencyImage = document.createElement('div');
+    newAgencyImage.classList.add("agency-image");
+
+    newAgencyImage.innerHTML = '<img src = "' + agency.logo + '">';
+    let newAgencyDesc = document.createElement('div');
+    newAgencyDesc.style.textDecoration = "none";
+    newAgencyDesc.classList.add("opis-agencije");
+
+    let newAgencyName = document.createElement('p');
+    newAgencyName.classList.add("ime-agencije");
+    newAgencyName.innerHTML = agency.naziv;
+    newAgencyDesc.appendChild(newAgencyName);
+    let newAgencyAdress = document.createElement('p');
+    newAgencyAdress.classList.add("adresa-agencije");
+    newAgencyAdress.innerHTML = agency.adresa;
+    newAgencyDesc.appendChild(newAgencyAdress);
+    let newAgencyNumber = document.createElement('p');
+    newAgencyNumber.classList.add("broj-telefona-agencije");
+    newAgencyNumber.innerHTML = agency.brojTelefona;
+    newAgencyDesc.appendChild(newAgencyNumber);
+    let newAgencyEmail = document.createElement('p');
+    newAgencyEmail.classList.add("email-agencije");
+    newAgencyEmail.innerHTML = agency.email;
+    newAgencyDesc.appendChild(newAgencyEmail);
+
+    newBoxCard.appendChild(newAgencyImage);
+    newBoxCard.appendChild(newAgencyDesc);
+
+    newMainBox.appendChild(newBoxCard);
+
+    let mainBoxes = document.querySelector(position);
+    mainBoxes.appendChild(newMainBox);
+    //document.querySelector(position).appendChild(carRow);
+}
+
+function ocitajStranicu(curPage) {
+    console.log(curPage);
+    window.location = "agencija.html";
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                //removeMainBoxes(".box");
+                agenciesID = [];
+                curBox = 0;
+                agencies = JSON.parse(request.responseText);
+                for (var id in agencies) {
+                    var agency = agencies[id];
+                    appendMainBox(".boxes", agency);
+                    agenciesID.push(id);
+                }
+            } else {
+                alert('Error occurred. Car could not be loaded.')
+            }
+        }
+    }
+    request.open('GET', firebaseUrl + '/agencjie.json');
+    request.send();
+    
+}
+
+
+/*
+function removeMainBoxes(position) {
+    var elem = document.querySelector(position);
     while (elem.firstChild) {
         elem.removeChild(elem.lastChild);
     }
 }
+*/
 
 
 
